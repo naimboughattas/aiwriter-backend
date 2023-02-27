3e336df60146247825d164ed303331fa3d9769e8
nboughattas <boughattas.naim@gmail.com>
2023-02-27 22:44:47 +0100

    CSRF Token exceptions

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        "https://aiwriter.so-sighty.fr/*",
+        "http://aiwriter.so-sighty.fr/*",
+        "http://aiwriter.so-sighty.fr/api/*"
     ];
 }
