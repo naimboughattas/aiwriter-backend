bc0af081f32209e9b25665be1836ad341629a1e8
nboughattas <boughattas.naim@gmail.com>
2023-02-27 22:53:47 +0100

    CSRF Token exceptions

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,6 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
+        'aiwriter.so-sighty.fr,https://aiwriter.so-sighty.fr,http://aiwriter.so-sighty.fr',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
