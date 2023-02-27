864b2e993202c68a007b41b92d4020ee8055b11f
nboughattas <boughattas.naim@gmail.com>
2023-02-27 22:49:09 +0100

    CSRF Token exceptions

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://aiwriter-vercel.vercel.app')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://aiwriter.so-sighty.fr')],
 
     'allowed_origins_patterns' => [],
 
